60cab72fdc26d5dc27a268be48870b4fa7554f92
Alan Schuster <amschus@umich.edu>
2024-07-31 14:14:10 -0400

    ...Updated 7/31

--- ReducerAlgorithm/reduceralg_partitioned.py
+++ ReducerAlgorithm/reduceralg_partitioned.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-def downsample_dataset(dataset, lon_bin_size, lat_bin_size):
-    # dataset: list of [lon, lat, data, ...]
-    # lon_bin_size: size of the longitude bin (e.g., 1 degree)
-    # lat_bin_size: size of the latitude bin (e.g., 1 degree)
-
-    # Empty dictionary to store bins
+def partition_data(dataset, lon_bin_size, lat_bin_size):
+    # Create an empty dictionary to store bins
     bins = {}
 
     for data_point in dataset:
@@ -17,39 +13,52 @@ def downsample_dataset(dataset, lon_bin_size, lat_bin_size):
 
         grid_key = (lon_grid, lat_grid)
 
-        # Initialize the bin if not already present
+        # Add data points to the corresponding bin
         if grid_key not in bins:
             bins[grid_key] = []
-
-        # Append the data point to the corresponding bin
         bins[grid_key].append(data_point)
 
-    # Calculate the mean of the data points within each bin
-    reduced_dataset = []
-    for grid_key, data_points in bins.items():
-        # Calculate the mean longitude and latitude
-        mean_lon = np.mean([point[0] for point in data_points])
-        mean_lat = np.mean([point[1] for point in data_points])
-        
-        # Take the first data value as the representative value
-        mean_data = data_points[0][2:]
+    return bins
 
-        # Create the reduced data point
-        reduced_data_point = [mean_lon, mean_lat] + mean_data
-        reduced_dataset.append(reduced_data_point)
+def map_data_to_bin(bins):
+    reduced_dataset = []
+
+    for bin_key, bin_data in bins.items():
+        if bin_data:
+            # Calculate the mean longitude, latitude, and other data
+            mean_lon = np.mean([point[0] for point in bin_data])
+            mean_lat = np.mean([point[1] for point in bin_data])
+            mean_data = [mean_lon, mean_lat]
+
+            # If there are additional data fields, calculate their mean as well
+            for i in range(2, len(bin_data[0])):
+                mean_value = np.mean([point[i] for point in bin_data])
+                mean_data.append(mean_value)
+
+            reduced_dataset.append(mean_data)
+
+    return reduced_dataset
+
+def downsample_dataset(dataset, lon_bin_size, lat_bin_size):
+    # Partition the data into bins
+    bins = partition_data(dataset, lon_bin_size, lat_bin_size)
+
+    # Map data to bins around the mean
+    reduced_dataset = map_data_to_bin(bins)
 
     return reduced_dataset
 
 # Example dataset
 dataset = [
-    [12.34, 56.78, 'data0'],
-    [12.35, 56.79, 'data1'],
-    [13.34, 57.78, 'data2'],
-    [13.35, 57.79, 'data3'],
+    [12.34, 56.78, 0.5],
+    [12.35, 56.79, 0.6],
+    [13.34, 57.78, 0.7],
+    [13.35, 57.79, 0.8],
 ]
 
-lon_bin_size = 2.0  # example - 2 degree longitude bins
-lat_bin_size = 2.0  # example - 2 degree latitude bins
+lon_bin_size = 1.0  # example - 1 degree longitude bins
+lat_bin_size = 1.0  # example - 1 degree latitude bins
+
 reduced_dataset = downsample_dataset(dataset, lon_bin_size, lat_bin_size)
 
 print("Original dataset size:", len(dataset))
